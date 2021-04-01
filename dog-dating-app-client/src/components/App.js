e1d39f456831db00872122457e1d975441589526
Robert Ricci <rmricci121@gmail.com>
2021-04-01 15:24:47 -0400

    This commit adds state and a form to the login page

--- dog-dating-app-client/src/components/App.js
+++ dog-dating-app-client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import About from "./About";
+import Login from "./Login";
 
 const App = () => {
   return (
     <div>
       <About />
+      <Login />
     </div>
   );
 };
