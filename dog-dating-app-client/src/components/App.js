f6b288f73aed4de98e122a8e4c5470f655f78185
Robert Ricci <rmricci121@gmail.com>
2021-04-06 13:12:15 -0400

    This commit adjusted the login page and added the signup page

--- dog-dating-app-client/src/components/App.js
+++ dog-dating-app-client/src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import About from "./About";
 import Login from "./Login";
+import SignUp from "./SignUp";
 
 const App = () => {
   return (
     <div>
       <About />
       <Login />
+      <SignUp />
     </div>
   );
 };
