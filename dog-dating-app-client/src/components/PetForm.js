21c95bd940f26e9714a61dd597604c3911173a04
catherinesmiley <catherinesmiley@gmail.com>
2021-04-12 00:07:10 -0400

    add actions folder, create add pet action, create handleonsubmit in pet form

--- dog-dating-app-client/src/components/PetForm.js
+++ dog-dating-app-client/src/components/PetForm.js
@@ -1,43 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux'
 
-const PetForm = () => {
-  return (
-    <div>
-        <form>
-            <h3>Name: </h3>
-            <input 
-              type="text"
-              name="name"
-            />
-            <h3>Age: </h3>
-            <input 
-              type="text"
-              name="age"
-            />
-            <h3>Location: </h3>
-            <input 
-              type="text"
-              name="location"
-            />
-            <h3>Biography: </h3>
-            <textarea name="biography"/>
-            <h3>Breed: </h3>
-            <input 
-              type="text"
-              name="breed"
-            />
-            <h3>Likes: </h3>
-            <textarea name="likes"/>
-            <h3>Dislikes: </h3>
-            <textarea name="dislikes"/>
-            <h3>Temperament: </h3>
-            <input 
-              type="text"
-              name="temperament"
-            />
-        </form>
-    </div>
-  );
+import { addPet } from '../actions/pets'
+
+class PetForm extends Component {
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    console.log("handleOnSubmit submitted!")
+  }
+
+  render() {
+    console.log("props", this.props)
+
+    return (
+      <div>
+          <form onSubmit={event => this.handleOnSubmit(event)}>
+              <h3>Name: </h3>
+              <input 
+                type="text"
+                name="name"
+              />
+              <h3>Age: </h3>
+              <input 
+                type="text"
+                name="age"
+              />
+              <h3>Location: </h3>
+              <input 
+                type="text"
+                name="location"
+              />
+              <h3>Biography: </h3>
+              <textarea name="biography"/>
+              <h3>Breed: </h3>
+              <input 
+                type="text"
+                name="breed"
+              />
+              <h3>Likes: </h3>
+              <textarea name="likes"/>
+              <h3>Dislikes: </h3>
+              <textarea name="dislikes"/>
+              <h3>Temperament: </h3>
+              <input 
+                type="text"
+                name="temperament"
+              />
+              <input type="submit"/>
+          </form>
+      </div>
+    );
+  }
 };
 
-export default PetForm;
+export default connect(null, { addPet })(PetForm);
--- dog-dating-app-client/src/reducers/petsReducer.js
+++ dog-dating-app-client/src/reducers/petsReducer.js
@@ -1,5 +1,7 @@
 export const petsReducer = (state = [], action) => {
     switch (action.type) {
+        case 'ADD_PET':
+            return [...state, action.pet]
         case 'FETCH_PETS':
             return action.pets
         default: 
